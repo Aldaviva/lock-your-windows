df3e19149ee08a813a2bd40e8b30319f264029f4
Ben Hutchison <ben@aldaviva.com>
2021-07-11 14:38:20 -0700

    #5: 'v8::Object::Set': no overloaded function takes 2 arguments

--- src/main/c++/bindings.cpp
+++ src/main/c++/bindings.cpp
@@ -10,8 +10,9 @@ void Lock(const Nan::FunctionCallbackInfo<v8::Value>& info){
 }
 
 void Init(v8::Local<v8::Object> exports){
-    exports->Set(Nan::New("isLocked").ToLocalChecked(), Nan::New<v8::FunctionTemplate>(IsLocked)->GetFunction(Nan::GetCurrentContext()).ToLocalChecked());
-    exports->Set(Nan::New("lock").ToLocalChecked(), Nan::New<v8::FunctionTemplate>(Lock)->GetFunction(Nan::GetCurrentContext()).ToLocalChecked());
+    v8::Local<v8::Context> context = exports->CreationContext();
+    exports->Set(context, Nan::New("isLocked").ToLocalChecked(), Nan::New<v8::FunctionTemplate>(IsLocked)->GetFunction(Nan::GetCurrentContext()).ToLocalChecked());
+    exports->Set(context, Nan::New("lock").ToLocalChecked(), Nan::New<v8::FunctionTemplate>(Lock)->GetFunction(Nan::GetCurrentContext()).ToLocalChecked());
 }
 
 NODE_MODULE(winlock, Init)
