032cc86a80c071bdb4ee23b5b575283e3104a378
Ben Hutchison <ben@aldaviva.com>
2023-03-22 01:13:08 -0700

    Fixed error C2039: "CreationContext": Not a member of "v8::Object" in Electron

--- src/main/c++/bindings.cpp
+++ src/main/c++/bindings.cpp
@@ -9,10 +9,9 @@ void Lock(const Nan::FunctionCallbackInfo<v8::Value>& info){
     lockWorkstation();
 }
 
-void Init(v8::Local<v8::Object> exports){
-    v8::Local<v8::Context> context = exports->CreationContext();
-    exports->Set(context, Nan::New("isLocked").ToLocalChecked(), Nan::New<v8::FunctionTemplate>(IsLocked)->GetFunction(Nan::GetCurrentContext()).ToLocalChecked());
-    exports->Set(context, Nan::New("lock").ToLocalChecked(), Nan::New<v8::FunctionTemplate>(Lock)->GetFunction(Nan::GetCurrentContext()).ToLocalChecked());
+NAN_MODULE_INIT(Init) {
+    Nan::Set(target, Nan::New("isLocked").ToLocalChecked(), Nan::New<v8::FunctionTemplate>(IsLocked)->GetFunction(Nan::GetCurrentContext()).ToLocalChecked());
+    Nan::Set(target, Nan::New("lock").ToLocalChecked(), Nan::New<v8::FunctionTemplate>(Lock)->GetFunction(Nan::GetCurrentContext()).ToLocalChecked());
 }
 
 NODE_MODULE(winlock, Init)
